998511400735ca99559efc31364a97732bf57b56
paultbr <paultheo65@gmail.com>
2015-06-13 11:37:16 -0500

    1
    1

--- validar.php
+++ validar.php
@@ -8,10 +8,12 @@ $username=$_POST['usuario'];
 $pass=$_POST['pass1'];
 
 
-
-$sql2=mysql_query("SELECT * FROM users WHERE user='$username'");
-if($f2=mysql_fetch_array($sql2)){
+/*----ADMINISTRADOR-----*/
+/*$sql2=mysql_query("SELECT * FROM tadministrador WHERE idadministrador ='$username'");*/
+$sql3=mysql_query("SELECT * FROM tusuario INNER JOIN tadministrador ON tusuario.idusuario = tadministrador.idusuario");
+if($f2=mysql_fetch_array($sql3)){
     if($pass==$f2['password']){
+        if($username==$f2['idadministrador'])
         echo '<script>alert("BIENVENIDO ADMINISTRADOR")</script> ';
 
         echo "<script>location.href='index23.php'</script>";
@@ -19,7 +21,44 @@ if($f2=mysql_fetch_array($sql2)){
     }
 }
 
+/*----AUXILIAR----*/
+$sql5=mysql_query("SELECT * FROM tusuario INNER JOIN tauxiliar ON tusuario.idusuario = tauxiliar.idusuario");
+if($f4=mysql_fetch_array($sql5)){
+    if($pass==$f4['password']){
+        if($username==$f4['idauxiliar'])
+            echo '<script>alert("BIENVENIDO AUXILIAR")</script> ';
 
+        echo "<script>location.href='index2336.php'</script>";
+
+    }
+}
+
+
+/*----APODERADO----*/
+$sql4=mysql_query("SELECT * FROM tusuario INNER JOIN tapoderado ON tusuario.idusuario = tapoderado.idusuario");
+if($f3=mysql_fetch_array($sql4)){
+    if($pass==$f3['password']){
+        if($username==$f3['idapoderado'])
+            echo '<script>alert("BIENVENIDO PADRE DE FAMILIA")</script> ';
+
+        echo "<script>location.href='index232.php'</script>";
+
+    }
+}
+
+/*----DOCENTE----*/
+$sql4=mysql_query("SELECT * FROM tusuario INNER JOIN tdocente ON tusuario.idusuario = tdocente.idusuario");
+if($f3=mysql_fetch_array($sql4)){
+    if($pass==$f3['password']){
+        if($username==$f3['idapoderado'])
+            echo '<script>alert("BIENVENIDO PROFESOR")</script> ';
+
+        echo "<script>location.href='index232.php'</script>";
+
+    }
+}
+
+/*
 
 
 $sql=mysql_query("SELECT * FROM users WHERE user='$username'");
@@ -40,7 +79,7 @@ if($f=mysql_fetch_array($sql)){
 }
 
 
-
+*/
 
 
 
