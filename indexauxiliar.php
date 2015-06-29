52c62288ef5134524618c9e8ea2f2339cd348971
paultbr <paultheo65@gmail.com>
2015-06-29 10:25:35 -0500

    v3
    las vantanas de auxiliar, docente y apoderado falta arreglar,

---
+++ indexauxiliar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <title>Fe y Alegria N°21</title>
+     <link rel="stylesheet" href="diseño/estilos.css">
+     <link rel="stylesheet" href="diseño/iconos.css">
+</head>
+<body>
+     <!--menu-->
+     <header>
+     	<nav>
+     		<ul>
+     			<li><a href="#"><span class=""><i class="icon icon-pencil"></i></span>Registrar</a>
+                    <ul>
+                    		<li><a href="usuario/administrador.php">Administrador</a></li>
+                    		<li><a href="usuario/docente.php">Docente</a></li>
+                   			<li><a href="usuario/apoderado.php">Apoderado</a></li>
+                    		<li><a href="usuario/auxiliar.php">Auxiliar</a></li>
+                    </ul>
+     			</li>
+     			<li><a href="#"><span class=""><i class="icon icon-users"></i></span>???????</a></li>
+     			<li><a href="#"><span class=""><i class="icon icon-mobile2"></i></span>???????</a></li>
+     			<li><a href="#"><span class=""><i class="icon icon-happy"></i></span>??????</a></li>
+     		</ul>
+     	</nav>
+     </header>
+    
+</body>
